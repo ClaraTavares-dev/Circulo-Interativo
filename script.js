2ee9f1c8f00f24523ac7fb02c4673670e86c3a33
Clarinhaserafim004 <Clarinhaserafim04@gmail.com>
2025-11-08 19:43:29 -0300

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const div = document.querySelector("div");
+
+function circulo(evento) {
+  const x = evento.clientX;
+  const y = evento.clientY;
+  div.style.left = x + "px"; // mesma ideia do css
+  div.style.top = y + "px";
+}
+window.addEventListener("mousemove", circulo);
+
